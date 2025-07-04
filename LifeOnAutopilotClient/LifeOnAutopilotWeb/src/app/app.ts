31a17438591213a6c36556fbc7983edb097d88d7
adam.sinkovics <adam.sinkovics@gmail.com>
2025-07-04 23:43:40 +0200

    feat: continuation

--- LifeOnAutopilotClient/LifeOnAutopilotWeb/src/app/app.ts
+++ LifeOnAutopilotClient/LifeOnAutopilotWeb/src/app/app.ts
@@ -1,25 +1,21 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {MatListModule} from '@angular/material/list';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { Calendar } from "../pages/calendar/calendar";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatToolbarModule, MatButtonModule, MatIconModule, MatSidenavModule, MatListModule],
+  imports: [RouterOutlet, MatToolbarModule, MatButtonModule, MatIconModule, MatSidenavModule, MatListModule, Calendar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected title = 'LifeOnAutopilot';
-  isMobile = signal(true);  
-  protected readonly fillerNav = Array.from({length: 50}, (_, i) => `Nav Item ${i + 1}`);
+  protected readonly title = 'LifeOnAutopilot';
+  protected readonly navButtons = ["General", "Calendar", "Finance", "Meals", "ToDo", "Notes", "Home"];
 
-  protected readonly fillerContent = Array.from(
-    {length: 50},
-    () =>
-      `My babe is sooo cute`,
-  );
+  isMobile = signal(false);  
 }
---
+++ LifeOnAutopilotClient/LifeOnAutopilotWeb/src/pages/calendar/calendar.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component ({
+    selector: 'calendar',
+    templateUrl: 'calendar.html',
+    styleUrl: 'calendar.css'
+})
+export class Calendar {}
