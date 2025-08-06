1048ed21a3485121e835ca304ff0cfbfdefbcf12
adam.sinkovics <adam.sinkovics@gmail.com>
2025-08-06 18:26:52 +0200

    fix: reorganizing project structure

---
+++ LifeOnAutopilotWeb/src/app/pages/finance/finance.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'loa-finance',
+  imports: [],
+  templateUrl: './finance.html',
+  styleUrl: './finance.css'
+})
+export class Finance {
+
+}
---
+++ LifeOnAutopilotWeb/src/app/pages/meals/meals.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'loa-meals',
+  imports: [],
+  templateUrl: './meals.html',
+  styleUrl: './meals.css'
+})
+export class Meals {
+
+}
