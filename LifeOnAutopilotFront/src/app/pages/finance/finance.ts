af5d14a748eaa5fd2cb65f19d6018bf276dd6ed0
adam.sinkovics <adam.sinkovics@gmail.com>
2025-08-26 21:07:41 +0200

    forgot file

--- LifeOnAutopilotFront/src/app/pages/finance/finance.ts
+++ LifeOnAutopilotFront/src/app/pages/finance/finance.ts
@@ -10,15 +10,10 @@ import { Component, inject } from '@angular/core';
 export class Finance {
   private http = inject(HttpClient);
   message = "";
-  myVar : any = { message:'' };
+  myVar: any = { message:'' };
   
   constructor()
   {
     this.http.get('http://localhost:5046/', { responseType:'json' }).subscribe(item => {this.myVar = item; console.log(item)});
   }
-}
-
-class Response 
-{
-  message: string = "";
 }
